23715d795feced59df21695131455c2b50af23d3
Schetann <schetann@gmail.com>
2015-04-22 13:13:28 +0300

    Update and rename ProgrammingAssignment2 to ProgrammingAssignment2.R

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
   set <- function(y) {
@@ -11,6 +16,8 @@ makeCacheMatrix <- function(x = matrix()) {
        setinverse = setinverse,
        getinverse = getinverse)
 }
+
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
